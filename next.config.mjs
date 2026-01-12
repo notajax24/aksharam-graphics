86464bb177305847ef567b87b71d703278e675d4
notajax24 <96714712+notajax24@users.noreply.github.com>
2026-01-12 21:29:53 +0530

    2nd commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nutcaseshop.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'googleusercontent.com', // For the map link if needed
+      }
+    ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
